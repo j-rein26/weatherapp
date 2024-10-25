a3a52e66ce18286a8a15d2357a92113239737d28
j-rein26 <grow435@gmail.com>
2024-10-25 12:28:28 -0400

    in main.py we processed we created dictionary and list to return weather conditions icons.

--- backend.py
+++ backend.py
@@ -7,19 +7,15 @@ load_dotenv()
 WEATHER_API = os.getenv("WEATHER_API")
 
 
-def get_data(place, forecast_days=None, type=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={WEATHER_API}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if type == "Temperature":
-        filtered_data = [dict['main']['temp'] for dict in filtered_data]
-    if type == "Weather Conditions":
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3,type="Weather Conditions"))
+    print(get_data(place="Tokyo", forecast_days=3))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.express as px
 from backend import get_data
+
+#add title text, text input, slide, selectbox, and subheader 
 st.title("Weather Forecast For Upcoming Days")
 place = st.text_input("Place: ")
 days_choice = st.slider("Days to Forecast",
@@ -12,9 +14,21 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for the next {days_choice} days in {place}")
 
+#only call the function if users types a place
+if place:
+# get the temperature and weather conditions
+    filtered_data = get_data(place, days_choice)
 
-data = get_data(place, days_choice, option)
+    if option == 'Temperature':
+        temperatures = [dict['main']['temp'] for dict in filtered_data]
+        dates = [dict['dt_txt'] for dict in filtered_data]
+    #Create temperature plot
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
 
-d, t = get_data(days_choice)
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+    if option == 'Weather Conditions':
+        images = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png', 
+                'Rain': 'images/rain.png', 'Snow': 'images/snow.png'}
+        weather_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+        image_paths = [images[condition] for condition in weather_conditions]
+        st.image(image_paths, width=115)
